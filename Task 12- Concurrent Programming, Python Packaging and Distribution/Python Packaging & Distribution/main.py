faede21cc0bc516dfc9472ff1e3bbb990afabf31
Pranay <khadepranay727@gmail.com>
2023-07-03 14:12:16 +0530

    Task Added

--- Task 12- Concurrent Programming, Python Packaging and Distribution/Python Packaging & Distribution/main.py
+++ Task 12- Concurrent Programming, Python Packaging and Distribution/Python Packaging & Distribution/main.py
@@ -1,10 +1,10 @@
 from mypckg import file2
 from mypckg import file1
-# from mypckg.module import factorial
+from mypckg.factorial import factorial
 
-# result = factorial(5)
-# print("The factorial is", result)  # Output: 120
+result = factorial(5)
+print("The factorial is", result)  # Output: 120
 
 file1.country()
 res = file2.sum(1, 2)
-print(res)
+print("The Sum is", res)
